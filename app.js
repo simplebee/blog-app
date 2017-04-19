1d6d12c11b747032661bdac157f081f517e57361
Shuiloon Yip <shuoodle@gmail.com>
2017-04-19 20:18:25 +0100

    Add blog schema and model

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ var express     = require('express'),
     mongoose    = require('mongoose'),
     bodyParser  = require('body-parser');
 
+// Express config
 app.set('view engine', 'ejs');
 
+// Mongoose schema and model
+mongoose.connect('mongodb://localhost/blog-app');
+
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  create: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model('Blog', blogSchema);
+
+// Routes
 app.get('/', function(req, res) {
   res.redirect('/blog');
 });
 
+// Index
 app.get('/blog', function(req, res) {
   res.render('index');
 });
